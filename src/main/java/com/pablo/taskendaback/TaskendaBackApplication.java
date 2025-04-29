e3d10c04c624be6df2f5de4b91fa1307f9917a32
Pablo <cuberoluquepablo@gmail.com>
2025-04-29 18:17:11 +0200

    Proyecto creado

---
+++ src/main/java/com/pablo/taskendaback/TaskendaBackApplication.java
@@ -0,0 +1,13 @@
+package com.pablo.taskendaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskendaBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskendaBackApplication.class, args);
+    }
+
+}
